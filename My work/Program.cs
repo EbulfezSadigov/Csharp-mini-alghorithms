8ecec67c971e6079a61819ac9435edf233248880
EbulfezSadigov <7ag0he8@code.edu.az>
2022-03-14 22:21:00 +0400

    Programs

--- My work/Program.cs
+++ My work/Program.cs
@@ -8,28 +8,28 @@ namespace My_work
         {
             #region Task 1
 
-            //int n = Convert.ToInt32(Console.ReadLine());
-            //int temp = 0;
-            //if (n==0||n==1)
-            //{
-            //    Console.WriteLine("Eded ne sadedir ne murekkeb");
-            //    return;
-            //}
-            //for (int i = 2; i < n / 2; i++)
-            //{
-            //    if (n % i == 0)
-            //    {
-            //        temp++;
-            //    }
-            //}
-            //if (temp == 0)
-            //{
-            //    Console.WriteLine("n is prime");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("n is not prime");
-            //}
+            int n = Convert.ToInt32(Console.ReadLine());
+            int temp = 0;
+            if (n == 0 || n == 1)
+            {
+                Console.WriteLine("Eded ne sadedir ne murekkeb");
+                return;
+            }
+            for (int i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    temp++;
+                }
+            }
+            if (temp == 0)
+            {
+                Console.WriteLine("n is prime");
+            }
+            else
+            {
+                Console.WriteLine("n is not prime");
+            }
             #endregion
 
             #region Task 2
@@ -53,22 +53,22 @@ namespace My_work
 
             #region Task 3
 
-            //int n = Convert.ToInt32(Console.ReadLine());
-            //int reverse = 0;
-            //int m = n;
-            //while (n>0)
-            //{
-            //    reverse = reverse * 10 + n%10;
-            //    n = n / 10;
-            //}
-            //if (m==reverse)
-            //{
-            //    Console.WriteLine("eded polindromdur");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("eded polindrom deyil");
-            //}
+            int n = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+            int m = n;
+            while (n > 0)
+            {
+                reverse = reverse * 10 + n % 10;
+                n = n / 10;
+            }
+            if (m == reverse)
+            {
+                Console.WriteLine("eded polindromdur");
+            }
+            else
+            {
+                Console.WriteLine("eded polindrom deyil");
+            }
             #endregion
         }
     }
